a47f16f97def43902df0e7e33ce761f6c99cac85
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-11-29 10:55:46 +0000

    Changed field input to text-sm

--- app/taxparameters/page.js
+++ app/taxparameters/page.js
@@ -262,7 +262,7 @@ export default function TaxParameters() {
           <div>
             <label className="block font-bold mb-1 text-sm">Tax Year</label>
             <input
-              className="border p-2 w-full text-xs"
+              className="border p-2 w-full text-sm"
               type="number"
               placeholder="Year"
               value={newParam.year}
@@ -282,7 +282,7 @@ export default function TaxParameters() {
                   .replace(/\b\w/g, (char) => char.toUpperCase())}{" "}
               </label>
               <input
-                className={`border p-2 w-full text-xs ${
+                className={`border p-2 w-full text-sm ${
                   errors[`incomeTax.${key}`] ? "border-red-500" : ""
                 }`}
                 type="number"
@@ -319,7 +319,7 @@ export default function TaxParameters() {
                   .replace(/\b\w/g, (char) => char.toUpperCase())}{" "}
               </label>
               <input
-                className={`border p-2 w-full text-xs ${
+                className={`border p-2 w-full text-sm ${
                   errors[`nationalInsurance.${key}`] ? "border-red-500" : ""
                 }`}
                 type="number"
